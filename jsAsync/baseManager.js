a5db87f9178bdfa24d3bafa084ecb3e0496f681d
yakintech <cagatayyildiz87@gmail.com>
2021-07-07 12:51:40 +0300

    js async

---
+++ jsAsync/baseManager.js
@@ -0,0 +1,30 @@
+
+
+
+
+async function getData(url) {
+
+    let products = [];
+
+    await fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            products = data;
+        });
+
+    let newProducts = products.splice(0, 5);
+
+    return newProducts
+
+}
+
+
+
+ getData('https://northwind.vercel.app/api/products').then((productsdata) => {
+
+    console.log(productsdata);
+
+ })
+
+
+
